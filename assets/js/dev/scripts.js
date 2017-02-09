e2a41f2ace9254f701106c68fa3ad3c160f6e562
brentrobbins <brent@variantstudios.com>
2017-02-08 17:15:27 -0700

    Tweaked the snipcard checkout css and js.

--- assets/js/dev/scripts.js
+++ assets/js/dev/scripts.js
@@ -15,23 +15,23 @@ $(document).ready(function() {
         "thankyou_message": "Thanks for your order!"
     });
 
-    $(".snipcart-summary").css('display', 'none');
-    Snipcart.subscribe('item.added', function (item) {
-        console.log("added");
-        $(".snipcart-summary").css('display', 'block');
-    });
-    Snipcart.subscribe('cart.ready', function (data) {
-        if(data.order !== undefined) {
-            console.log(data.order);
-            if (data.order.items.length != 0) {
-            //console.log('yes');
-                $(".snipcart-summary").css('display', 'block');
-            } else {
-            //console.log('no');
-                $(".snipcart-summary").css('display', 'none');
-            }
-        }
-    });
+    //$(".snipcart-summary").css('display', 'none');
+    // Snipcart.subscribe('item.added', function (item) {
+    //     console.log("added");
+    //     $(".snipcart-summary").css('display', 'block');
+    // });
+    // Snipcart.subscribe('cart.ready', function (data) {
+    //     if(data.order !== undefined) {
+    //         console.log(data.order);
+    //         if (data.order.items.length != 0) {
+    //         //console.log('yes');
+    //             $(".snipcart-summary").css('display', 'block');
+    //         } else {
+    //         //console.log('no');
+    //             $(".snipcart-summary").css('display', 'none');
+    //         }
+    //     }
+    // });
 
     // Snipcart.execute('bind', 'order.completed', function(order) {
     //     var url = '/thankyou?order=' + order.token;
